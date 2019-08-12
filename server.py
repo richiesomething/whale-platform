9b0957918d583f5eea3749ad0a0de47d4c32bff4
richiesomething <hern784@usc.edu>
2019-08-11 17:22:03 -0700

    run python3 server.py to run. everything is in one file

--- server.py
+++ server.py
@@ -1,39 +1,92 @@
-from flask import Flask
+from flask import Flask,Blueprint, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_restful import Resource, Api
-db = SQLAlchemy()
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-def create_app():
-    app = Flask(__name__)
-    api = Api(app)
+app = Flask(__name__)
+app.config['SECRET_KEY']='thisismysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
 
-    app.config['SECRET_KEY']='thisismysecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String())
+    name = db.Column(db.String(100))
 
-    db.init_app(app)
 
-    login_manager =  LoginManager()
-    login_manager.login_view = 'pages.login'
-    login_manager.init_app(app)
+db.init_app(app)
 
-    from models import User
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+login_manager =  LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
-    from pages import pages as pagesBlueprint
-    app.register_blueprint(pagesBlueprint)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    return app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email=request.form.get('email')
+    password=request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Email or Password is incorrect')
+        return redirect(url_for('login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('profile'))
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash('Email address already exists.')
+        return redirect(url_for('signup'))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
     def main():
-        app = create_app()
-        app.run(debug=config.debug)
+        db.create_all()
         app.run(debug=True)
-
     main()
 
 #     def db_test_main():
